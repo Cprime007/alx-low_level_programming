139423ac572c671138d72f9ed2cd5622129e331b
Cprime007 <collinsabide@icloud.com>
2022-12-14 09:14:28 -0800

    My first c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,23 @@
-n: Always 0 (Success)
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-        int i;
-        int ch;
+	int i;
+	int ch;
 
-        ch = 122;
-        i = 26;
-        while (i > 0)
-        {
-                putchar(ch);
-                ch--;
-                i--;
-        }
-        putchar(10);
-        return (0);
+	ch = 122;
+	i = 26;
+	while (i > 0)
+	{
+		putchar(ch);
+		ch--;
+		i--;
+	}
+	putchar(10);
+	return (0);
 }
