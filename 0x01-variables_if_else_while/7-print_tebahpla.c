d8e0fc263582d94e0d582cafb0fa27e77abac8f6
Cprime007 <collinsabide@icloud.com>
2022-12-14 09:02:49 -0800

    My first c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int ch;
+
+	ch = 122;
+	i = 26;
+	while (i > 0)
+	{
+		putchar(ch);
+		ch--;
+		i--;
+	}
+	putchar(10);
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+
