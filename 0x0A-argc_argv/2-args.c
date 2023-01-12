74551add1c3349be1195892838292705afcd0194
Cprime007 <collinsabide@icloud.com>
2023-01-12 03:30:15 -0800

    argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,45 +1,18 @@
-#include "main.h"
-
-void print_array_elements(char *str);
+#include <stdio.h>
 
 /**
-* main - Prints all arguments it receives
-*
-* @argc: Length of @argv, integer
-*
-* @argv: Array of strings of the arguments of this program
-*
-* Return: 0, success
-*/
+ * main - program that prints all arguments it receives.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0
+ */
 
 int main(int argc, char *argv[])
 {
 	int i;
-
 	for (i = 0; i < argc; i++)
 	{
-		print_array_elements(argv[i]);
-		_putchar('\n');
+		printf("%s\n", argv[i]);
 	}
-
 	return (0);
-}
-
-/**
-* print_array_elements - Prints all char of a string
-*
-* @str: Pointer to string
-*
-* Return: void
-*/
-
-void print_array_elements(char *str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
 }
